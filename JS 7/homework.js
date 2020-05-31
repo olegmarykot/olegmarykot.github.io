b7a0505c722e0b11fefc702490022d4e69ef1616
olegmarykot <olkooo.m@gmail.com>
2020-05-31 21:53:49 +0300

    js6

--- JS 7/homework.js
+++ JS 7/homework.js
@@ -1,3 +1,4 @@
+// Мінімальне значення, максимальне, сума позитивних, добуток негативних, кількість позитивних і негативних
 var arr = [2, -3, -28, 1, 5, 10, -11, 13, 23, 4];
 
 var min = arr[0];
@@ -44,3 +45,74 @@ for ( var i = 0; i < arr.length; i++) {
 }
 console.log ("negative=", neg.length);
 console.log ("positive=", pos.length);
+
+// Підрахунок букв
+function countSymbols (word) {
+var result = {};
+for (var i in word) {
+    if (result.hasOwnProperty(word[i])) {
+        result[word[i]]++;
+    }
+    else {
+        result[word[i]]=1;
+    }
+}
+
+return result;
+}
+
+var word = "abracadabra"
+countSymbols (word);
+
+// Перемішати вміст
+function ShuffleArray(array) {
+    array.sort(function() {
+        return (0.5 - Math.random());
+    });
+    return array;
+}
+
+var array = ["apple", "banana", "melon", "pear"];
+
+console.log(array);
+console.log(ShuffleArray(array));
+
+//кількість парних і непарних чисел
+function oddEven (number){
+var result = {
+    odd: 0,
+    even: 0
+};
+var count = number.toString();
+for ( i = 0; i < count.length; i++) {
+    if (Number(count[i]) % 2 == 0) {
+        result.even++;
+    }
+    else {
+        result.odd++;
+    }
+}
+return result;
+}
+
+//числа фібоначчі
+function Fibonacci (num){
+    if (num > 0) {
+    var result = [];
+    var a=1;
+    for (i = 0; i < num; i++){
+    if (result.length < 2) {
+        result.push(a);
+    }
+    else {
+    a = result[i-1] + result [i-2];
+    result.push(a);
+    }
+}
+    }
+    else {
+        console.log("null")
+    }
+return result;
+    }
+console.log (Fibonacci(7));
